143d284af3527f439864e4309d8f3fc2482a727a
Eudes89 <eudesmith@gmail.com>
2024-02-18 17:29:26 -0300

    auteração no componente loginBtn

--- components/login-btn.jsx
+++ components/login-btn.jsx
@@ -4,11 +4,11 @@ export default function LoginBtn() {
   const { data: session } = useSession()
 
   const handleGoogleAuth = async () => {
-        
+    
     // callbackurl com u minusculo redireciona para a api do google
     // callbackUrl com U maiusculo redireciona o usuário depois de logado.
     await signIn('google', {
-        callbackurl: 'http://eadev.vercel.app/api/auth/callback/google',
+        callbackurl: 'https://eadev.vercel.app/api/auth/callback/google',
         callbackUrl: '/'
     })
     
@@ -25,7 +25,7 @@ export default function LoginBtn() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => handleGoogleAuth()}>Sign in</button>
+      <button onClick={handleGoogleAuth}>Sign in</button>
     </>
   )
 }
